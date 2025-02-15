d23e424dea67385b3e0c08d26597ff2f57a05c47
16KnighT <tinpottoby@gmail.com>
2025-02-15 13:34:37 +0000

    enum xforms

--- example.py
+++ example.py
@@ -25,27 +25,60 @@ class TimeUnit(Enum):
     TWO_WEEKS = "1209600"
     FOUR_WEEKS = "2419200"
 
-def append_valid_xforms(valid_xforms, stat):
-    if stat.value == "callsanswered":
-        valid_xforms.extend([
-            "sumringday", "sumringhour", "avhourring", "avdayring"
-        ])
-    elif stat.value == "callspend":
-        valid_xforms.extend([
-            "maxspendhour", "maxspendday", "sumspendhour", "sumsspendday"
-        ])
+class BaseXform(Enum):
+
+    @classmethod
+    def supports_stat(cls, stat: str) -> bool:
+        return stat in cls.__related_stats__
+
+    @classmethod
+    def valid_xform(cls):
+        return [member.value for member in cls]
 
 
-def get_babble_stats(device, stat: Optional[Stats] = None, xform=None, timespan: Optional[TimeUnit]=None, offset: Optional[TimeUnit]=None):
-    valid_xforms = [
-        "meanhour", "minhour", "maxhour", "sumbillday", "sumbillhour",
-        "sumdurationday", "sumdurationhour", "countday", "counthour",
-        "avhourbillsec", "avavdaybillsec", "avhourduration", "avavdayduration"
-        ]
+class ValidXforms(BaseXform):
+    __related_stats__ = ["calls", "callsmade", "callsanwered", "callsreceived" "callsunanswered", "callspend"]
+    MEAN_HOUR = "meanhour"
+    MIN_HOUR = "minhour"
+    MAX_HOUR = "maxhour"
+    SUM_BILL_DAY = "sumbillday"
+    SUM_BILL_HOUR = "sumbillhour"
+    SUM_DURATION_DAY = "sumdurationday"
+    SUM_DURATION_HOUR = "sumdurationhour"
+    COUNT_DAY = "countday"
+    COUNT_HOUR = "counthour"
+    AV_HOUR_BILL_SEC = "avhourbillsec"
+    AV_DAY_BILL_SEC = "avdaybillsec"
+    AV_HOUR_DURATION = "avhourduration"
+    AV_DAY_DURATION = "avdayduration"
 
-    append_valid_xforms(valid_xforms, stat)
+class ValidXformsAnswered(BaseXform):
+    __related_stats__ = ["callsanswered"]
+    SUM_RING_DAY = "sumringday"
+    SUM_RING_HOUR = "sumringhour"
+    AV_HOUR_RING = "avhourring"
+    AV_DAY_RING = "avdayring"
+
+class ValidXformsSpend(BaseXform):
+    __related_stats__ = ["callspend"]
+    MAX_SPEND_HOUR = "maxspendhour"
+    MAX_SPEND_DAY = "maxspendday"
+    SUM_SPEND_HOUR = "sumspendhour"
+    SUM_SPEND_DAY = "sumspendday"
+
+def check_xform(xform, stat):
+    for enum_class in BaseXform.__subclasses__():
+        if enum_class.supports_stat(stat.value):
+            supported_xforms = enum_class.valid_xform()
+            if xform.value in enum_class.valid_xform(): return True
+
+    return False
+
+def get_babble_stats(device, stat: Optional[Stats] = None, xform="", timespan: Optional[TimeUnit]=None, offset: Optional[TimeUnit]=None):
     
-    if xform not in valid_xforms:
+    if check_xform(xform, stat):
+        xform = xform.value
+    else:
         xform = ""
         print("invalid xform")
 
@@ -70,6 +103,6 @@ def get_babble_stats(device, stat: Optional[Stats] = None, xform=None, timespan:
 devices = requests.get("https://www.babblevoice.com/api/device", headers=headers).json()
 print(f"number of devices on the server: {len(devices["rows"])}")
 
-leighton_stats = get_babble_stats("9022@omniis.babblevoice.com", Stats.CALLS_ANSWERED, timespan=TimeUnit.FIVE_DAYS, xform="sumringhour")
+leighton_stats = get_babble_stats("9022@omniis.babblevoice.com", Stats.CALLS_ANSWERED, timespan=TimeUnit.FIVE_DAYS, xform=ValidXformsAnswered.SUM_RING_HOUR)
 print(leighton_stats)
 

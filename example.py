b6808393445502c229518f683d9a8964f7e6499d
16KnighT <tinpottoby@gmail.com>
2025-02-17 11:50:44 +0000

    queue stats

--- example.py
+++ example.py
@@ -10,13 +10,18 @@ headers = {
     "Authorization": f"Bearer {MY_TOKEN}"
 }
 
-class Stats(Enum):
+class DeviceStats(Enum):
     CALLS = "calls"
     CALLS_MADE = "callsmade"
     CALLS_ANSWERED = "callsanswered"
     CALLS_RECEIVED = "callsereceived"
     UNANSWERED = "callsunanswered"
     SPEND = "callspend"
+
+class QueueStats(Enum):
+    ANSWERED = "answered"
+    ABANDONED = "abandoned"
+    QUEUE = "queue"
 
 class TimeUnit(Enum):
     DAY = "86400"
@@ -35,8 +40,12 @@ class BaseXform(Enum):
     def valid_xform(cls):
         return [member.value for member in cls]
 
+class BaseDeviceXform(BaseXform):
+    pass
+class BaseQueueXform(BaseXform):
+    pass
 
-class ValidXforms(BaseXform):
+class DeviceXforms(BaseDeviceXform):
     __related_stats__ = ["calls", "callsmade", "callsanwered", "callsreceived" "callsunanswered", "callspend"]
     MEAN_HOUR = "meanhour"
     MIN_HOUR = "minhour"
@@ -52,35 +61,97 @@ class ValidXforms(BaseXform):
     AV_HOUR_DURATION = "avhourduration"
     AV_DAY_DURATION = "avdayduration"
 
-class ValidXformsAnswered(BaseXform):
+class XformsDeviceAnswered(BaseDeviceXform):
     __related_stats__ = ["callsanswered"]
     SUM_RING_DAY = "sumringday"
     SUM_RING_HOUR = "sumringhour"
     AV_HOUR_RING = "avhourring"
     AV_DAY_RING = "avdayring"
 
-class ValidXformsSpend(BaseXform):
+class XformsDeviceSpend(BaseDeviceXform):
     __related_stats__ = ["callspend"]
     MAX_SPEND_HOUR = "maxspendhour"
     MAX_SPEND_DAY = "maxspendday"
     SUM_SPEND_HOUR = "sumspendhour"
     SUM_SPEND_DAY = "sumspendday"
 
-def check_xform(xform, stat):
-    for enum_class in BaseXform.__subclasses__():
+class XformQueueAnswered(BaseQueueXform):
+    __related_stats__ = ["answered"]
+    MAX_HOUR_WAIT = "maxhourwait"
+    MAX_DAY_WAIT = "maxdaywait"
+    MAX_HOUR_BRIDGE = "maxhourbridge"
+    MAX_DAY_BRIDGE = "maxdaybridge"
+    MIN_HOUR_WAIT = "minhourwait"
+    MIN_DAY_WAIT = "mindaywait"
+    MIN_HOUR_BRIDGE = "minhourbridge"
+    MIN_DAY_BRIDGE = "mindaybridge"
+    MEAN_HOUR_WAIT = "meanhourwait"
+    MEAN_DAY_WAIT = "meandaywait"
+    MEAN_HOUR_BRIDGE = "meanhourbridge"
+    MEAN_DAY_BRIDGE = "meandaybridge"
+    SUM_HOUR_WAIT = "sumhourwait"
+    SUM_DAY_WAIT = "sumdaywait"
+    SUM_HOUR_BRIDGE = "sumhourbridge"
+    SUM_DAY_BRIDGE = "sumdaybridge"
+    COUNT_HOUR = "counthour"
+    COUNT_DAY = "countday"
+
+class XformQueueAbandoned(BaseQueueXform):
+    __related_stats__ = ["abandoned"]
+    MAX_HOUR_WAIT = "maxhourwait"
+    MAX_DAY_WAIT = "maxdaywait"
+    MIN_HOUR_WAIT = "minhourwait"
+    MIN_DAY_WAIT = "mindaywait"
+    MEAN_HOUR_WAIT = "meanhourwait"
+    MEAN_DAY_WAIT = "meandaywait"
+    SUM_HOUR_WAIT = "sumhourwait"
+    SUM_DAY_WAIT = "sumdaywait"
+    COUNT_HOUR = "counthour"
+    COUNT_DAY = "countday"
+
+class XformQueue(BaseQueueXform):
+    __related_stats__ = ["queue"]
+    MAX_HOUR_MEMBER = "maxhourmember"
+    MAX_DAY_MEMBER = "maxdaymember"
+    MAX_HOUR_CONSUMER = "maxhourconsumer"
+    MAX_DAY_CONSUMER = "maxdayconsumer"
+    MAX_HOUR_CALLER = "maxhourcaller"
+    MAX_DAY_CALLER = "maxdaycaller"
+    MAX_HOUR_WAITING = "maxhourwaiting"
+    MAX_DAY_WAITING = "maxdaywaiting"
+    MAX_HOUR_BRIDGE = "maxhourbridge"
+    MAX_DAY_BRIDGE = "maxdaybridge"
+    COUNT_HOUR = "counthour"
+    COUNT_DAY = "countday"
+    MIN_HOUR_MEMBER = "minhourmember"
+    MIN_DAY_MEMBER = "mindaymember"
+    MIN_HOUR_CONSUMER = "minhourconsumer"
+    MIN_DAY_CONSUMER = "mindayconsumer"
+    MEAN_HOUR_CALLER = "meanhourcaller"
+    MEAN_DAY_CALLER = "meandaycaller"
+    MEAN_HOUR_WAITING = "meanhourwaiting"
+    MEAN_DAY_WAITING = "meandaywaiting"
+    MEAN_HOUR_BRIDGE = "meanhourbridge"
+    MEAN_DAY_BRIDGE = "meandaybridge"
+
+def check_xform(xform, stat, cls):
+    for enum_class in cls.__subclasses__():
         if enum_class.supports_stat(stat.value):
             supported_xforms = enum_class.valid_xform()
             if xform.value in enum_class.valid_xform(): return True
 
     return False
 
-def get_babble_stats(device, stat: Optional[Stats] = None, xform="", timespan: Optional[TimeUnit]=None, offset: Optional[TimeUnit]=None):
+def get_device_stats(device, stat: Optional[DeviceStats] = None, xform="", timespan: Optional[TimeUnit]=None, offset: Optional[TimeUnit]=None):
     
-    if check_xform(xform, stat):
+    if check_xform(xform, stat, BaseDeviceXform):
         xform = xform.value
     else:
         xform = ""
         print("invalid xform")
+
+    print(xform)
+    print(stat.value)
 
     url = "https://www.babblevoice.com/api/device/stats"
     params = {
@@ -100,9 +171,38 @@ def get_babble_stats(device, stat: Optional[Stats] = None, xform="", timespan: O
         print("Error fetching stats")
         return None
 
+def get_queue_stats(group, domain, stat=None, xform=None, timespan: Optional[TimeUnit]=None, offset: Optional[TimeUnit]=None):
+
+    if check_xform(xform, stat, BaseQueueXform):
+        xform = xform.value
+    else:
+        xform = ""
+        print("invalid xform")
+
+    url = "https://www.babblevoice.com/api/queue/stats"
+    params = {
+        "g": group,  
+        "d": domain,
+        "s": stat if xform is not None else '',
+        "x": xform, 
+        "t": timespan.value if timespan is not None else '',  
+        "o": offset.value if offset is not None else '',
+        "of": "json"
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print("Error fetching stats")
+        return None
+
+
 devices = requests.get("https://www.babblevoice.com/api/device", headers=headers).json()
 print(f"number of devices on the server: {len(devices["rows"])}")
 
-leighton_stats = get_babble_stats("9022@omniis.babblevoice.com", Stats.CALLS_ANSWERED, timespan=TimeUnit.FIVE_DAYS, xform=ValidXformsAnswered.SUM_RING_HOUR)
-print(leighton_stats)
+#leighton_stats = get_device_stats("9022@omniis.babblevoice.com", DeviceStats.CALLS_ANSWERED, timespan=TimeUnit.FIVE_DAYS, xform=ValidXformsAnswered.SUM_RING_HOUR)
+#print(leighton_stats)
 
+queue_stats = get_queue_stats("Queue", "omniis.babblevoice.com", QueueStats.ABANDONED, XformQueue.SUM_DAY_WAIT, TimeUnit.DAY)
+print(queue_stats)
